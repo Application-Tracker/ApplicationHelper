bcd5eb82dc8c4389740bf1db804f8aa816be30df
Katrina Henderson <henderson.katrina33@gmail.com>
2022-03-15 11:50:01 -0700

    built out userController.login -- not yet tested

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,40 @@
+const db = require('../server/models/appModel');
+
+const PG_URI = db.URI;
 const userController = {};
+
+//login controler
+userController.login = (req, res, next) => {
+  console.log('in userController.auth', req.body);
+  //get username and password from request body -- eventually will encrpt password 
+  const { username, password} = req.body;
+  //validate user and password - if either are missing/invalid, send error message
+  if (!username || !password) {
+    return res.status(400).send('All fields are required.');
+  }
+  //if username and password ARE both present, make query to db
+  const userQuery = `SELECT * FROM users WHERE username = $1`;
+  
+  //I THINK this is the right set up to use the $1 and all that
+  db.query(userQuery, [username])
+    .then((data) => {
+      //store data on res.locals.userData -- should contain username and password
+      res.locals.userData;
+
+      //if userData.password === password and same for username, login successful
+      if (res.locals.userData.username === username && res.locals.userData.password === password) {
+        console.log('Login successful!');
+        next();
+      //if username or password not found in database, login unsuccessful
+      } else {
+        console.log('Login unsuccessful');
+        return res.status(400).send('Invalid credentials, please try logging in again.');
+      };
+    })
+    .catch((err) => {
+      next({
+        log: 'Error in userController.login',
+        message: { err: 'Error in userController.login'},
+      })
+    });
+};
--- server/models/appModel.js
+++ server/models/appModel.js
@@ -5,3 +5,20 @@ const URI = process.env.PG_URI;
 const pool = new Pool({
   connectionString: URI,
 });
+
+//Hey Nick! I wasn't sure about this next bit so I'm justing leaving this here commented out -- I need to do some research
+// const client = new Client({
+//   connectionString: PG_URI
+// });
+
+// client.connect();
+
+
+//export object that contains query property -- is a function that returns invocation of pool.query() after logging query
+//provides access point to database
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  }
+};

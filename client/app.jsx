c9cc22504e38cc142ef3f3aec71520120cc71cf3
NAusintO <nickaozawa@gmail.com>
2022-03-16 10:32:20 -0700

    prior to phoenix/jiwon-login merge

--- client/app.jsx
+++ client/app.jsx
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar.jsx';
-=======
-import React from 'react';
->>>>>>> 484f3d7220dec31d699b5ae58481476f34ed4dfe
 import MainContainer from './components/MainContainer.jsx';
 import Header from './components/Header.jsx';
 
 const App = () => {
 
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
+  const [isAuthenticated, setIsAuthenticated] = useState(false); 
+
+
 
   useEffect(() => {
     fetch('/api/user/authenticate')
